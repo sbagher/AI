70a2f9f4f73bfb5af399f90f26b0bf6d8a37623f
sbagher <sbagher@ut.ac.ir>
2023-06-02 16:01:52 +0330

    Update c01p.py

--- c01p.py
+++ c01p.py
@@ -8,3 +8,8 @@ print('\nQ1: Number of nodes: ', len(G.nodes()))
 
 # Q2: Number of edges
 print('\nQ2: Number of edges:', len(G.edges()))
+
+# Q3: In-degree, out-degree and degree of the first five nodes
+print('\nQ3: In-degree, out-degree and degree of the first five nodes')
+for n in list(G.nodes)[0:5]:
+    print('node:', n, 'in-degree:', G.in_degree(n), 'out-degree:', G.out_degree(n), 'degree:', G.degree(n))
