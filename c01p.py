dc4bae26e72f9c90b85bcf05ae4a800e25ed73cd
sbagher <sbagher@ut.ac.ir>
2023-06-02 16:12:37 +0330

    Update c01p.py

--- c01p.py
+++ c01p.py
@@ -12,11 +12,18 @@ print('\nQ2: Number of edges:', len(G.edges()))
 # Q3: In-degree, out-degree and degree of the first five nodes
 print('\nQ3: In-degree, out-degree and degree of the first five nodes')
 for n in list(G.nodes)[0:5]:
-    print('node:', n, 'in-degree:', G.in_degree(n), 'out-degree:', G.out_degree(n), 'degree:', G.degree(n))
+    print('node:', n, '{in-degree:', G.in_degree(n), 'out-degree:', G.out_degree(n), 'degree:', G.degree(n),"}")
 
 # Q4: Number of source nodes
-sn = []
+src = []
 for n in G.nodes():
     if G.in_degree(n) == 0:
-        sn.append(n)
-print('\nQ4: Number of source nodes:', len(sn))
+        src.append(n)
+print('\nQ4: Number of source nodes: ', len(src))
+
+# Q5: Number of sink nodes
+snk = []
+for n in G.nodes():
+    if G.out_degree(n) == 0:
+        snk.append(n)
+print('\nQ5: Number of sink nodes: ', len(snk))
