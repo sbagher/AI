f3c68a78f48e8808b5033db997fc045111c56385
sbagher <sbagher@ut.ac.ir>
2023-06-02 15:39:24 +0330

    Update c01p.py

--- c01p.py
+++ c01p.py
@@ -1,3 +1,7 @@
 import networkx as nx
 
+# read the edges from the 'email-Eu-core.txt' file
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+
+# Q1: Number of nodes
+print('Q1: Number of nodes: ', len(G.nodes()))
