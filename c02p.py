6fe1cda84c509deaf1e29186b1f26d4219d7e90e
sbagher <sbagher@ut.ac.ir>
2023-06-02 23:15:53 +0330

    Update c02p.py

--- c02p.py
+++ c02p.py
@@ -1,1 +1,7 @@
 import networkx as nx
+
+G = nx.read_edgelist('soc-Epinions1.txt', create_using=nx.DiGraph())
+
+print("Number of nodes:", G.number_of_nodes())
+print("Number of edges:", G.number_of_edges())
+
