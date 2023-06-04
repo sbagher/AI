459e8b217bdf59466845ad5478a1b0768cc3dd10
sbagher <sbagher@ut.ac.ir>
2023-06-04 10:34:26 +0330

    O

--- Chapter01/p01.py
+++ Chapter01/p01.py
@@ -9,4 +9,5 @@ import networkx as nx
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 
 # P01: Number of nodes
-print('P01: Number of nodes:', len(G.nodes()))
+print('Problem: 01, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Number of nodes:', len(G.nodes()))
