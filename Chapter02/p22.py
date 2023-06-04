38e5446b05a96b33d097cb82108e00425ab995b0
sbagher <sbagher@ut.ac.ir>
2023-06-04 21:03:50 +0330

    Create p22.py

---
+++ Chapter02/p22.py
@@ -0,0 +1,12 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: AI Applications in Social Networks
+# Assignment: Problem: 01, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+
+# read the edges from the 'email-Eu-core.txt' file
+G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+
+print('Problem: 01, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Number of nodes:', len(G.nodes()))
