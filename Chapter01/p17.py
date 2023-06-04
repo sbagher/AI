1bccf95417ec0a15f4c6b2cd93f60c901e2c3a58
sbagher <sbagher@ut.ac.ir>
2023-06-04 19:06:35 +0330

    Update p17.py

--- Chapter01/p17.py
+++ Chapter01/p17.py
@@ -10,9 +10,8 @@ import random
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 
 print('Problem: 17, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
-
-# Q17: Number of nodes in In(v) for five random nodes
 print('Number of nodes in In(v) for five random nodes')
+
 random_nodes = random.sample(list(G.nodes()), 5)
 
 for node in random_nodes:
