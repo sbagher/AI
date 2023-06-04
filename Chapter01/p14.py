5d7174725783093d961e7865009e514927eef29b
sbagher <sbagher@ut.ac.ir>
2023-06-04 15:42:35 +0330

    Update p14.py

--- Chapter01/p14.py
+++ Chapter01/p14.py
@@ -12,14 +12,16 @@ G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 print('Problem: 14, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
 print('Is the graph weakly connected? If so, compute the weakly connected component size distribution')
 
-if nx.is_strongly_connected(G):
-    print("The graph is strongly connected.")
+if nx.is_weakly_connected(G):
+    print("The graph is weakly connected.")
 else:
-    sccs = list(nx.strongly_connected_components(G))
+    wccs = list(nx.weakly_connected_components(G))
 
-    scc_sizes = [len(scc) for scc in sccs]
-    scc_size_counts = Counter(scc_sizes)
+    wcc_sizes = [len(wcc) for wcc in wccs]
+    wcc_size_counts = Counter(wcc_sizes)
 
-    print("The graph is not strongly connected. Strongly connected component size distribution:")
-    for size, count in sorted(scc_size_counts.items()):
+    print("The graph is not weakly connected. Weakly connected component size distribution:")
+    for size, count in sorted(wcc_size_counts.items()):
         print(f"\t{size}: {count}")
+print ("")
+n=G.nodes('808')
