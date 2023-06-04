9833e45778839f74d00aaa2f2af7acd119868cc0
sbagher <sbagher@ut.ac.ir>
2023-06-04 14:37:31 +0330

    Create p12.py

---
+++ Chapter01/p12.py
@@ -0,0 +1,20 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: AI Applications in Social Networks
+# Assignment: Problem: 12, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+from collections import Counter
+
+# read the edges from the 'email-Eu-core.txt' file
+G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+
+print('Problem: 12, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+
+diameter=0
+for n in G.nodes:
+    lengths = nx.shortest_path_length(G, n)
+    m=max(lengths.values())
+    if m>diameter:
+        diameter=m
+print('Diameter:',diameter)
