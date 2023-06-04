f41033dabe43a443c03f1c50d3398ab49f1831a5
sbagher <sbagher@ut.ac.ir>
2023-06-04 19:06:28 +0330

    Update p19.py

--- Chapter01/p19.py
+++ Chapter01/p19.py
@@ -1,7 +1,7 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
-# Assignment: Problem: 17, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+# Assignment: Problem: 19, Chapter: 01, Book: "Practical Social Network Analysis with Python"
 
 import networkx as nx
 import random
@@ -9,12 +9,10 @@ import random
 # read the edges from the 'email-Eu-core.txt' file
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 
-print('Problem: 18, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Problem: 19, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Clustering coefficient for five random nodes')
 
-# Q18: Number of nodes in Out (v) for five random nodes
-print('Number of nodes in Out (v) for five random nodes')
 random_nodes = random.sample(list(G.nodes()), 5)
 
 for node in random_nodes:
-    outv = list(nx.bfs_successors(G,node))
-    print(f"\tNumber of nodes in In({node}): {len(outv)}")
+    print(f"\tClustering coefficient of node {node}: {nx.clustering(G, node)}")
