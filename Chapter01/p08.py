3f97f3f2221d5838704d0ba60c00a4b71a105c94
sbagher <sbagher@ut.ac.ir>
2023-06-04 11:38:04 +0330

    Update p08.py

--- Chapter01/p08.py
+++ Chapter01/p08.py
@@ -1,7 +1,7 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
-# Assignment: Problem: 07, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+# Assignment: Problem: 08, Chapter: 01, Book: "Practical Social Network Analysis with Python"
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -10,13 +10,13 @@ import numpy as np
 # read the edges from the 'email-Eu-core.txt' file
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 
-print('Problem: 07, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
-print('In-degree distribution')
+print('Problem: 08, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Out-degree distribution')
 
-dgsq = [G.in_degree(n) for n in nx.nodes(G)]
+dgsq = [G.out_degree(n) for n in nx.nodes(G)]
 plt.hist(dgsq, bins=max(dgsq))
-plt.title('In-Degree Histogram')
-plt.xlabel('In-Degree')
+plt.title('Out-Degree Histogram')
+plt.xlabel('Out-Degree')
 plt.ylabel('Frequency')
 plt.show()
 
@@ -25,7 +25,7 @@ for d in dgsq:
     freq[d] += 1
 degree_prob=freq/sum(freq)
 plt.bar(range(0,max(dgsq)+1),degree_prob,color ='maroon')
-plt.title('In-Degree distribution Histogram')
-plt.xlabel('In-Degree')
+plt.title('Out-Degree distribution Histogram')
+plt.xlabel('Out-Degree')
 plt.ylabel('Probability')
 plt.show()
