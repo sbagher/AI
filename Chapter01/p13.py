2eaa9093c288d5c8fef0637aeebfad4f15021aad
sbagher <sbagher@ut.ac.ir>
2023-06-04 14:50:15 +0330

    Create p13.py

---
+++ Chapter01/p13.py
@@ -0,0 +1,26 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: AI Applications in Social Networks
+# Assignment: Problem: 13, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+from collections import Counter
+
+# read the edges from the 'email-Eu-core.txt' file
+G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+
+print('Problem: 13, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Is the graph strongly connected? If so, compute the strongly connected component size distribution')
+
+if nx.is_strongly_connected(G):
+    print("The graph is strongly connected.")
+else:
+    sccs = list(nx.strongly_connected_components(G))
+
+    scc_sizes = [len(scc) for scc in sccs]
+    scc_size_counts = Counter(scc_sizes)
+
+    print("The graph is not strongly connected. Strongly connected component size distribution:")
+    for size, count in sorted(scc_size_counts.items()):
+        print(f"\t{size}: {count}")
+
