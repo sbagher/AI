3e0321068ada869ad86f8ac457265a429dbeaa42
sbagher <sbagher@ut.ac.ir>
2023-06-04 17:44:11 +0330

    Update p15.py

--- Chapter01/p15.py
+++ Chapter01/p15.py
@@ -9,8 +9,31 @@ import networkx as nx
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 
 print('Problem: 15, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
-print('Number of bridge edges')
 
-G_undirected = G.to_undirected(as_view=True)
-bridges = list(nx.bridges(G_undirected))
-print(f"\nQ15: Number of bridge edges: {len(bridges)}")
+# Algorithm #1
+UG = G.to_undirected()
+bridges = list(nx.bridges(UG))
+print(f"First Algorithm (Fast): Convert to undirected graph and use bridges() function)")
+print(f"\tNumber of bridge edges: {len(bridges)}")
+
+# Algorithm #2
+# create weakly connected components
+print(f"Second Algorithm (Slow, Please waite...): Remvove each edge and check: is the new graph connected too?")
+
+nwccs = len(list(nx.weakly_connected_components(G)))
+num_bridge_edges = 0
+edges = list(G.edges())
+
+# iterate over edges and check if they are bridge edges
+for u, v in edges:
+    # remove edge (u, v) from the graph
+    G.remove_edge(u, v)
+    
+    # check if the graph is still weakly connected
+    if len(list(nx.weakly_connected_components(G))) != nwccs:
+        num_bridge_edges += 1
+    
+    # add edge (u, v) back to the graph
+    G.add_edge(u, v)
+# print the number of bridge edges
+print(f"\tNumber of bridge edges: {num_bridge_edges}")
