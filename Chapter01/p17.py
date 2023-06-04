3ec19cadc8e924f40170819fa608071975ce6e65
sbagher <sbagher@ut.ac.ir>
2023-06-04 18:46:01 +0330

    Update p17.py

--- Chapter01/p17.py
+++ Chapter01/p17.py
@@ -12,10 +12,9 @@ G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 print('Problem: 17, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
 
 # Q17: Number of nodes in In(v) for five random nodes
-print('\nQ17: Number of nodes in In(v) for five random nodes')
+print('Number of nodes in In(v) for five random nodes')
 random_nodes = random.sample(list(G.nodes()), 5)
 
 for node in random_nodes:
-    in_neighbors = list(G.predecessors(node))
-    print(f"\tNumber of nodes in In({node}): {len(list(in_neighbors))}")
-    print (in_neighbors)
+    inv = list(nx.bfs_predecessors(G,node))
+    print(f"\tNumber of nodes in In({node}): {len(inv)}")
