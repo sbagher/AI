30cfbf7188361d246d45a9efb5dc6000e16aeccd
sbagher <135139435+sbagher@users.noreply.github.com>
2023-06-04 10:12:55 +0330

    Add files via upload

---
+++ Chapter01/p04.py
@@ -0,0 +1,12 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: AI Applications in Social Networks
+# Assignment: Problem: 04, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+
+# read the edges from the 'email-Eu-core.txt' file
+G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+# P04: Number of source nodes
+src = [n for n in G.nodes() if G.in_degree(n) == 0]
+print('\nP04: Number of source nodes: ', len(src))
