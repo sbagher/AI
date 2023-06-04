156128deee9a40600591fe794791c87df52cb54e
sbagher <sbagher@ut.ac.ir>
2023-06-04 11:44:41 +0330

    Update p10.py

--- Chapter01/p10.py
+++ Chapter01/p10.py
@@ -1,15 +1,15 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
-# Assignment: Problem: 09, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+# Assignment: Problem: 10, Chapter: 01, Book: "Practical Social Network Analysis with Python"
 
 import networkx as nx
 
 # read the edges from the 'email-Eu-core.txt' file
 G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
 
-print('Problem: 09, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
-print('Average degree, average in-degree and average out-degree')
+print('Problem: 10, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Distance between five pairs of random nodes')
 
 print("\tAverage degree:", sum([G.degree(n) for n in nx.nodes(G)]) / len(G.nodes))
 print("\tAverage in-degree:", sum([G.in_degree(n) for n in nx.nodes(G)]) / len(G.nodes))
