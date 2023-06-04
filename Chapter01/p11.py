69d3c262ac6b4e467ac26676b61298867dd5b2c9
sbagher <sbagher@ut.ac.ir>
2023-06-04 11:59:32 +0330

    Create p11.py

---
+++ Chapter01/p11.py
@@ -0,0 +1,31 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: AI Applications in Social Networks
+# Assignment: Problem: 10, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+import random
+
+# read the edges from the 'email-Eu-core.txt' file
+G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+
+print('Problem: 10, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
+print('Distance between five pairs of random nodes')
+
+nodes = list(G.nodes)
+node_pairs = []
+for i in range(5):
+    pair = random.sample(nodes, 2)
+    node_pairs.append(tuple(pair))
+
+print(f"Nodes:\n\t{node_pairs}\nResult:")
+
+for pair in node_pairs:
+    source, target = pair
+    try:
+        distance = nx.shortest_path_length(G, source=source, target=target)
+        print(f"\t Distance between nodes {source} and {target}: {distance}")
+    except nx.NodeNotFound as e:
+        print(f"\t Node not found: {e}")
+    except nx.NetworkXNoPath as e:
+        print(f"\t No path found between nodes {source} and {target}")
