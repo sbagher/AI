199b3ce14effdb2c365603f3f0d5f23c8cb11eca
sbagher <sbagher@ut.ac.ir>
2023-06-04 21:09:18 +0330

    Update p22.py

--- Chapter02/p22.py
+++ Chapter02/p22.py
@@ -1,12 +1,34 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
-# Assignment: Problem: 01, Chapter: 01, Book: "Practical Social Network Analysis with Python"
+# Assignment: Problem: 22, Chapter: 02, Book: "Practical Social Network Analysis with Python"
 
 import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
 
-# read the edges from the 'email-Eu-core.txt' file
-G = nx.read_edgelist('email-Eu-core.txt', create_using=nx.DiGraph())
+# read the edges from the 'soc-Epinions1.txt' file
+G = nx.read_edgelist('soc-Epinions1.txt', create_using=nx.DiGraph())
 
-print('Problem: 01, Chapter: 01, Book: "Practical Social Network Analysis with Python"\n')
-print('Number of nodes:', len(G.nodes()))
+print('Problem: 22, Chapter: 02, Book: "Practical Social Network Analysis with Python"\n')
+print('Compute the in-degree and out-degree distributions and plot the power law for each of these distributions.')
+
+def run_in_out(in_out,color,lable):
+    max_degree = max(in_out.values())
+    freq=np.zeros(max_degree + 1)
+    for degree in in_out.values():
+        freq[degree] += 1
+    degree_prob=freq/sum(freq)
+    # Plot the in-degree distribution
+    plt.figure(figsize=(10, 5))
+    plt.plot(range(len(degree_prob)), degree_prob, color)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel(f'{lable}-degree')
+    plt.ylabel('Probability')
+    plt.title(f'{lable}-degree Distribution')
+    plt.grid(True)
+    plt.show()
+
+run_in_out(dict(G.in_degree()),'bo','In')
+run_in_out(dict(G.out_degree()),'ro','Out')
