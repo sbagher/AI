9b066c105ceb19d55b69c8761f956915eec87e95
sbagher <sbagher@ut.ac.ir>
2023-06-04 22:14:10 +0330

    Update p23.py

--- Chapter02/p23.py
+++ Chapter02/p23.py
@@ -18,10 +18,29 @@ covered in these BFS runs as shown in Fig. 2.7. Create one figure for the forwar
 and one for the backward BFS. Howmany nodes are in the OUT and IN components?\n\
 How many nodes are in the TENDRILS component?\n')
 
-random_nodes = random.sample(list(G.nodes()), 10)
+random_nodes = random.sample(list(G.nodes()), 50)
 inv, outv = [], []
 
 for node in random_nodes:
     inv.append(len(list(nx.bfs_predecessors(G,node))))
     outv.append(len(list(nx.bfs_successors(G,node))))
 
+def plot_BFS(covered_nodes,color,lable):
+    max_covered_nodes = max(covered_nodes)
+    freq=np.zeros(max_covered_nodes + 1)
+    for covered_node in covered_nodes:
+        freq[covered_node] += 1
+    degree_prob=freq/sum(freq)
+    # Plot the in-degree distribution
+    plt.figure(figsize=(10, 5))
+    plt.plot(degree_prob, range(len(degree_prob)), color)
+    plt.xscale('linear')
+    plt.yscale('log')
+    plt.xlabel(f'{lable}-degree')
+    plt.ylabel('Probability')
+    plt.title(f'{lable}-degree Distribution')
+    plt.grid(True)
+    plt.show()
+
+plot_BFS (inv,'bo','In')
+plot_BFS (outv,'ro','Out')
