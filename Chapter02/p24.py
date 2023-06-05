e424255b846beeb1a1b4df2be4e14b6d5bba8994
sbagher <sbagher@ut.ac.ir>
2023-06-05 21:11:34 +0330

    Update p24.py

--- Chapter02/p24.py
+++ Chapter02/p24.py
@@ -1,9 +1,21 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
-# Assignment: Problem: 23, Chapter: 02, Book: "Practical Social Network Analysis with Python"
+# Assignment: Problem: 24, Chapter: 02, Book: "Practical Social Network Analysis with Python"
 
 import networkx as nx
+import random
+import matplotlib.pyplot as plt
+from collections import Counter
+
+# read the edges from the 'soc-Epinions1.txt' file
+G = nx.read_edgelist('soc-Epinions1.txt', create_using=nx.DiGraph(), comments='#')
+
+print('Problem: 24, Chapter: 02, Book: "Practical Social Network Analysis with Python"\n')
+print('What is the probability that a path exists between two nodes chosen uniformly \
+from the graph? What if the node pairs are only drawn from the WCC of the two \
+networks? Compute the percentage of node pairs that were connected in each of \
+these cases.\n')
 
 N = nx.DiGraph()
 nx.add_path(N, [0, 1, 2, 3, 4, 7])
