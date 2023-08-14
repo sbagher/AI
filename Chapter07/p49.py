4b817463b841a13da296df09c67f488c0592c850
sbagher <sbagher@ut.ac.ir>
2023-08-14 20:38:34 +0330

    Create p49.py

---
+++ Chapter07/p49.py
@@ -0,0 +1,4 @@
+# Name: Saeed Baghershahi
+# Student Number: 102501002
+# Class: AI Applications in Social Networks
+# Assignment: Problem: 49, Chapter: 07, Book: "Practical Social Network Analysis with Python"
