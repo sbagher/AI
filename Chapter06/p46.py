3155d0ca40c6e0f70923580d862243d41bfee102
sbagher <sbagher@ut.ac.ir>
2023-08-14 20:36:13 +0330

    Update p46.py

--- Chapter06/p46.py
+++ Chapter06/p46.py
@@ -1,4 +1,4 @@
 # Name: Saeed Baghershahi
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
-# Assignment: Problem: 45, Chapter: 05, Book: "Practical Social Network Analysis with Python"
+# Assignment: Problem: 46, Chapter: 06, Book: "Practical Social Network Analysis with Python"
