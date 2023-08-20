36487f696271cfae0fa4726ef446b346fe15b46b
sbagher <sbagher@ut.ac.ir>
2023-08-20 17:27:54 +0330

    Update p39.py

--- Chapter05/p39.py
+++ Chapter05/p39.py
@@ -2,3 +2,10 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 39, Chapter: 05, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+# read the edges from the 'com-friendster.ungraph.txt' file
+G = nx.read_edgelist('com-friendster.ungraph.txt', create_using=nx.DiGraph(), comments='#')
