97358781ea5fb0c5abb2f10ec074d316bc89c116
sbagher <sbagher@ut.ac.ir>
2023-08-20 18:30:36 +0330

    Update p39.py

--- Chapter05/p39.py
+++ Chapter05/p39.py
@@ -6,6 +6,14 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+
+start_time = time.time()
 
 # read the edges from the 'com-friendster.ungraph.txt' file
 G = nx.read_edgelist('com-friendster.ungraph.txt', create_using=nx.DiGraph(), comments='#')
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(elapsed_time)
