68002fd1b8d9fd7922452e827cc6b3d73f718ace
sbagher <sbagher@ut.ac.ir>
2023-08-22 18:42:12 +0330

    Update p28.py

--- Chapter03/p28.py
+++ Chapter03/p28.py
@@ -2,3 +2,6 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 28, Chapter: 03, Book: "Practical Social Network Analysis with Python"
+
+x = round(5.76543, 1)
+print(x)
