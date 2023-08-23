3bb63b64d1863d43b81ea62ccfe34952e597c958
sbagher <sbagher@ut.ac.ir>
2023-08-23 12:00:37 +0330

    Update p27.py

--- Chapter03/p27.py
+++ Chapter03/p27.py
@@ -3,16 +3,15 @@
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 27, Chapter: 03, Book: "Practical Social Network Analysis with Python"
 
+
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 
-print('Problem: 27, Chapter: 03, Book: "Practical Social Network Analysis with Python"\n')
-print('Clustering coefﬁcient distributions\n')
+print('Problem: 25, Chapter: 03, Book: "Practical Social Network Analysis with Python"\n')
+print('Degree distributions\n')
 
 # read the edges from the 'CA-AstroPh.txt' file
 rwg = nx.read_edgelist('CA-AstroPh.txt', create_using=nx.DiGraph(), comments='#')
-
 rwg_nodes = len(rwg.nodes())
 rwg_edges = len(rwg.edges())
 ind = [rwg.in_degree(n) for n in nx.nodes(rwg)]
@@ -24,19 +23,11 @@ cg = nx.directed_configuration_model (ind,outd,seed=10)
 
 plt.rcParams["figure.figsize"] = (15,5)
 def show_dist(g,i,t):
-    clustering_coeffs = nx.clustering(g)
-    counter = {}
-    for t in np.arange(0, 1.1, 0.1):
-        counter[round(t,1)] = 0
-
-    for coeff in clustering_coeffs.values():
-        c = round(coeff,1)
-        counter[c] += 1
-
+    dgsq = [g.degree(n) for n in nx.nodes(g)]
     plt.subplot(1, 3, i)
-    plt.bar(np.arange(0, 1.1, 0.1), counter.values(), color ='green', width = 0.4)
-    plt.title(f'Clustering Coefficient Distribution Histogram for \n{t}')
-    plt.xlabel('Path Length')
+    plt.hist(dgsq, bins=max(dgsq), color ='red')
+    plt.title(f'Degree Distribution Histogram for \n{t}')
+    plt.xlabel('Degree')
     plt.ylabel('Frequency')
 show_dist(rwg,1,"Real World Graph")
 show_dist(rg,2,"Erdös–Rényi Random Graph")
