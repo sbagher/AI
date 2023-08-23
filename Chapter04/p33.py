6148849246ce62e52f98b5417d33e881850030d0
sbagher <sbagher@ut.ac.ir>
2023-08-23 18:01:36 +0330

    Update p33.py

--- Chapter04/p33.py
+++ Chapter04/p33.py
@@ -2,3 +2,12 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 33, Chapter: 04, Book: "Practical Social Network Analysis with Python"
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import Counter
+
+print('Problem: 33, Chapter: 04, Book: "Practical Social Network Analysis with Python"\n')
+print('Degree distribution\n')
+
+# read the edges from the 'CA-AstroPh.txt' file
+rwg = nx.read_edgelist('CA-GrQc.txt', create_using=nx.DiGraph(), comments='#')
