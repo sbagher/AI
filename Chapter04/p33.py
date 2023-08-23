7856a9ca3688d8e3b0c5995a3a406f49cba926aa
sbagher <sbagher@ut.ac.ir>
2023-08-23 19:01:43 +0330

    Update p33.py

--- Chapter04/p33.py
+++ Chapter04/p33.py
@@ -2,9 +2,9 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 33, Chapter: 04, Book: "Practical Social Network Analysis with Python"
+
 import networkx as nx
 import matplotlib.pyplot as plt
-from collections import Counter
 
 print('Problem: 33, Chapter: 04, Book: "Practical Social Network Analysis with Python"\n')
 print('Degree distribution\n')
@@ -13,9 +13,18 @@ print('Degree distribution\n')
 rwg = nx.read_edgelist('CA-GrQc.txt', create_using=nx.DiGraph(), comments='#')
 
 rwg_nodes = len(rwg.nodes())
-rwg_edges = len(rwg.edges())
 
-swg = nx.watts_strogatz_graph(rwg_nodes, 5 , 0.5)
+swg = nx.watts_strogatz_graph(rwg_nodes, 5, 0.5, seed=10)
 
-rwg_edges = len(swg.edges())
-rwg_nodes = len(swg.nodes())
+plt.rcParams["figure.figsize"] = (10,5)
+def show_dist(g,i,t):
+    dgsq = [g.degree(n) for n in nx.nodes(g)]
+    plt.subplot(1, 2, i)
+    plt.hist(dgsq, bins=max(dgsq), color ='red')
+    plt.title(f'Degree Distribution Histogram for \n{t}')
+    plt.xlabel('Degree')
+    plt.ylabel('Frequency')
+
+show_dist(rwg,1,"Real World Graph")
+show_dist(swg,2,"Small World Graph")
+plt.show()
