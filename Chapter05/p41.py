cbc556c6c68fadd1299448221f98495ab737b639
sbagher <sbagher@ut.ac.ir>
2023-08-24 17:04:41 +0330

    Update p41.py

--- Chapter05/p41.py
+++ Chapter05/p41.py
@@ -5,36 +5,31 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
+from collections import Counter
 
 print('Problem: 41, Chapter: 05, Book: "Practical Social Network Analysis with Python"\n')
 print('WCC size distributions\n')
 
-# Read the edges from the 'com-dblp.ungraph.txt' instead of 'com-friendster.ungraph.txt' file,
+# Read the edges from the 'ca-HepPh.txt' instead of 'com-friendster.ungraph.txt' file,
 # because friendster dataset needs huge memory and computing. Two mentioned files have 
 # same characteristics (see https://snap.stanford.edu/data/index.html part "Networks with ground-truth communities")
 # It seems that friendster dataset needs GraphX on Hadoop or Spark. So it can not run on my laptop!
-rwg = nx.read_edgelist('com-dblp.ungraph.txt', create_using=nx.Graph(), comments='#')
+rwg = nx.read_edgelist('ca-HepPh.txt', create_using=nx.DiGraph(), comments='#')
 
 plt.rcParams["figure.figsize"] = (15,5)
 def show_dist(g,i,t):
-    l=np.zeros(100,dtype=np.ulonglong)
-    for n in g.nodes:
-        lengths = nx.shortest_path_length(g, n)
-        for j in list(lengths.values()):
-            l[j] += 1
+    wccs = list(nx.weakly_connected_components(g))
 
-    maxl=0
-    for j in range (99,0,-1):
-        if l[j] != 0:
-            maxl=j
-            break
+    wcc_sizes = [len(wcc) for wcc in wccs]
+    wcc_size_counts = Counter(wcc_sizes)
+    l = map(str, wcc_size_counts.keys())
+    l = list(l)
 
-    plt.subplot(1, 1, i)
-    plt.bar(range (0,maxl), l[0:maxl], color ='maroon', width = 0.4)
-    plt.title(f'Shortest Path Length Distribution Histogram for \n{t}')
-    plt.xlabel('Path Length')
+    plt.subplot(1, 2, i)
+    plt.bar(l, wcc_size_counts.values(), color ='blue', width = 0.4)
+    plt.title(f'WCC Size Distribution Histogram for \n{t}')
+    plt.xlabel('WCC Size')
     plt.ylabel('Frequency')
 
-show_dist(rwg,1,"DBLP collaboration network")
+show_dist(rwg,1,"(High Energy Physics - Phenomenology) collaboration network dataset")
 plt.show()
