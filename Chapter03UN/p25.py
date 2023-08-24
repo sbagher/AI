a2c8f5469fe80043914aa0cb46158e3dcb1db395
sbagher <sbagher@ut.ac.ir>
2023-08-24 13:40:12 +0330

    Update p25.py

--- Chapter03UN/p25.py
+++ Chapter03UN/p25.py
@@ -9,7 +9,7 @@ print('Problem: 25, Chapter: 03, Book: "Practical Social Network Analysis with P
 print('Erdös–Rényi random graph (G(n, m): Generate a random instance of this model by using the number of nodes and edges as the real world graph.\n')
 
 # read the edges from the 'CA-AstroPh.txt' file
-rwg = nx.read_edgelist('CA-AstroPh.txt', create_using=nx.DiGraph(), comments='#')
+rwg = nx.read_edgelist('CA-AstroPh.txt', create_using=nx.Graph(), comments='#')
 
 rwg_nodes = len(rwg.nodes())
 rwg_edges = len(rwg.edges())
