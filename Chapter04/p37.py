1560c650db9c86329b2332ee7a818b751ee42f60
sbagher <sbagher@ut.ac.ir>
2023-08-24 04:45:01 +0330

    Delete p37.py

--- Chapter04/p37.py
+++
@@ -1,4 +0,0 @@
-# Name: Saeed Baghershahi
-# Student Number: 102501002
-# Class: AI Applications in Social Networks
-# Assignment: Problem: 37, Chapter: 04, Book: "Practical Social Network Analysis with Python"
