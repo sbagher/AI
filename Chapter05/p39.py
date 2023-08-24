deae85a4274201518acba1a2be963f34e43a0e48
sbagher <sbagher@ut.ac.ir>
2023-08-24 17:04:34 +0330

    Update p39.py

--- Chapter05/p39.py
+++ Chapter05/p39.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 print('Problem: 39, Chapter: 05, Book: "Practical Social Network Analysis with Python"\n')
 print('Degree distribution\n')
 
-# Read the edges from the 'CA-HepTh.txt' instead of 'com-friendster.ungraph.txt' file,
+# Read the edges from the 'ca-HepPh.txt' instead of 'com-friendster.ungraph.txt' file,
 # because friendster dataset needs huge memory and computing. Two mentioned files have 
 # same characteristics (see https://snap.stanford.edu/data/index.html part "Networks with ground-truth communities")
 # It seems that friendster dataset needs GraphX on Hadoop or Spark. So it can not run on my laptop!
-rwg = nx.read_edgelist('CA-HepTh.txt', create_using=nx.DiGraph(), comments='#')
+rwg = nx.read_edgelist('ca-HepPh.txt', create_using=nx.DiGraph(), comments='#')
 
 plt.rcParams["figure.figsize"] = (10,5)
 def show_dist(g,i,t):
@@ -24,5 +24,5 @@ def show_dist(g,i,t):
     plt.xlabel('Degree')
     plt.ylabel('Frequency')
 
-show_dist(rwg,1,"DBLP collaboration network")
+show_dist(rwg,1,"(High Energy Physics - Phenomenology) collaboration network dataset")
 plt.show()
