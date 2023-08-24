8cc7cc24934c2c068f2ca3baebeedeb52297b388
sbagher <sbagher@ut.ac.ir>
2023-08-24 11:38:39 +0330

    Update p40.py

--- Chapter05/p40.py
+++ Chapter05/p40.py
@@ -2,3 +2,39 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 40, Chapter: 05, Book: "Practical Social Network Analysis with Python"
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+print('Problem: 39, Chapter: 05, Book: "Practical Social Network Analysis with Python"\n')
+print('Degree distribution\n')
+
+# Read the edges from the 'com-dblp.ungraph.txt' instead of 'com-friendster.ungraph.txt' file,
+# because friendster dataset needs huge memory and computing. Two mentioned files have 
+# same characteristics (see https://snap.stanford.edu/data/index.html part "Networks with ground-truth communities")
+# It seems that friendster dataset needs GraphX on Hadoop or Spark. So it can not run on my laptop!
+rwg = nx.read_edgelist('com-dblp.ungraph.txt', create_using=nx.DiGraph(), comments='#')
+
+plt.rcParams["figure.figsize"] = (15,5)
+def show_dist(g,i,t):
+    l=np.zeros(100,dtype=np.ulonglong)
+    for n in g.nodes:
+        lengths = nx.shortest_path_length(g, n)
+        for j in list(lengths.values()):
+            l[j] += 1
+
+    maxl=0
+    for j in range (99,0,-1):
+        if l[j] != 0:
+            maxl=j
+            break
+
+    plt.subplot(1, 1, i)
+    plt.bar(range (0,maxl), l[0:maxl], color ='maroon', width = 0.4)
+    plt.title(f'Shortest Path Length Distribution Histogram for \n{t}')
+    plt.xlabel('Path Length')
+    plt.ylabel('Frequency')
+
+show_dist(rwg,1,"DBLP collaboration network")
+plt.show()
