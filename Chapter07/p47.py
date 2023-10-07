7012c5fd9ab0313fcbdaab6a44cd389f64ae1f1e
sbagher <sbagher@ut.ac.ir>
2023-10-07 22:44:22 +0330

    Update p47.py

--- Chapter07/p47.py
+++ Chapter07/p47.py
@@ -11,7 +11,7 @@ print('Problem: 47, Chapter: 07, Book: "Practical Social Network Analysis with P
 print('Calculate the count and fraction of triads of each type in this network.\n')
 
 # read the edges from the 'soc-sign-epinions.txt' file
-g = nx.read_weighted_edgelist('soc-sign-epinions.txt', create_using=nx.Graph(), comments='#')
+g = nx.read_edgelist('soc-sign-epinions.txt', create_using=nx.Graph(), comments='#', nodetype=int, data=(("sign", int),))
 
 for a, b in g.edges():
-    print (a,b,g[a][b]['weight'])
+    print (a,b,g[a][b]['sign'])
