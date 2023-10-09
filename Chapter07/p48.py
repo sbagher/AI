3db09709c08937246be2d6e19e6a3bfc8d31a3f2
sbagher <sbagher@ut.ac.ir>
2023-10-09 20:39:10 +0330

    Update p48.py

--- Chapter07/p48.py
+++ Chapter07/p48.py
@@ -2,3 +2,28 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 48, Chapter: 07, Book: "Practical Social Network Analysis with Python"
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+print('Problem: 48, Chapter: 07, Book: "Practical Social Network Analysis with Python"\n')
+print('Calculate the fraction of positive and negative edges in the graph. Let the fraction\n\
+of positive edges be p. Assuming that each edge of a triad will independently be.\n\
+assigned a positive sign with probability p and a negative sign with probability\n\
+1 − p, calculate the probability of each type of triad.\n')
+
+# read the edges from the 'soc-sign-epinions.txt' file
+g = nx.read_edgelist('soc-sign-epinions.txt', create_using=nx.MultiGraph(), comments='#', nodetype=int, data=(("sign", int),))
+g.remove_edges_from(nx.selfloop_edges(g))
+
+p, n = 0, 0
+for u, v, keys, sign in g.edges(data="sign", keys=True):
+    if sign == 1:
+        p += 1
+    else:
+        n += 1
+t = p + n
+
+print (f"fraction of positive edges:{p/t}")
+print (f"fraction of negative edges:{n/t}")
