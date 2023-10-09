894ec9c1afdb0c69dc0d1d7c06d9815f2b7db1ce
sbagher <sbagher@ut.ac.ir>
2023-10-09 22:51:09 +0330

    Update p49.py

--- Chapter07/p49.py
+++ Chapter07/p49.py
@@ -2,3 +2,17 @@
 # Student Number: 102501002
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 49, Chapter: 07, Book: "Practical Social Network Analysis with Python"
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+print('Problem: 49, Chapter: 07, Book: "Practical Social Network Analysis with Python"\n')
+print('Now, analyse a simple generative model of signed networks by running simulations\n\
+of the dynamic process on small networks in the following manner. Create a\n\
+complete network on 10 nodes. For each edge, choose a sign with uniform probability.\n\
+Run this dynamic process for a million iterations. Repeat this process 100 times.\n\
+What fraction of these networks are balanced?')
+
+g = nx.complete_graph(10)
