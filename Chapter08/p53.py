ba4015178f67970224ab957a6f4e8d49c9050dc3
sbagher <sbagher@ut.ac.ir>
2023-10-12 10:37:53 +0330

    Update p53.py

--- Chapter08/p53.py
+++ Chapter08/p53.py
@@ -8,4 +8,6 @@ import networkx as nx
 import numpy as np
 
 print('Problem: 53, Chapter: 08, Book: "Practical Social Network Analysis with Python"\n')
-print()
+print('Simulate the effect of spending on the two graphs. First, read in the two graphs\n\
+again and assign the initial configurations as before.Now, before the decision process,\n\
+you purchase Rs. k of ads and go through the decision process of counting votes.')
