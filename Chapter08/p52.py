809c6c1bcc6dba8e76a2471d657ef0c47d09e9c1
sbagher <sbagher@ut.ac.ir>
2023-10-12 16:05:23 +0330

    Update p52.py

--- Chapter08/p52.py
+++ Chapter08/p52.py
@@ -10,3 +10,7 @@ import numpy as np
 print('Problem: 52, Chapter: 08, Book: "Practical Social Network Analysis with Python"\n')
 print('Perform these configurations and iterations, and compute who wins in the first\n\
 graph, and by how much? Similarly, compute the votes for the second graph.')
+
+g1 = nx.gnm_random_graph(10000, 100000, seed=10, directed=False)
+g2 = nx.barabasi_albert_graph(10000,10,10)
+
