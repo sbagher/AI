75b1a41f0f86b71b38eea1d8ed59f614b4b01899
sbagher <sbagher@ut.ac.ir>
2023-10-12 16:27:55 +0330

    Update p52.py

--- Chapter08/p52.py
+++ Chapter08/p52.py
@@ -14,3 +14,14 @@ graph, and by how much? Similarly, compute the votes for the second graph.')
 g1 = nx.gnm_random_graph(10000, 100000, seed=10, directed=False)
 g2 = nx.barabasi_albert_graph(10000,10,10)
 
+def election(g):
+    c0 = {8,9}
+    c1 = {0,2,4,6}
+    c2 = {1,3,5,7}
+    for _ in range (10):
+        for n in g.nodes():
+            t = n % 10
+            if t in c0:
+                
+            else:
+                continue
