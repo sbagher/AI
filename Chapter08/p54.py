6cae05e7067b22c338c22bfa1e0d28b34997a0ef
sbagher <sbagher@ut.ac.ir>
2023-10-12 10:38:48 +0330

    Update p54.py

--- Chapter08/p54.py
+++ Chapter08/p54.py
@@ -8,4 +8,7 @@ import networkx as nx
 import numpy as np
 
 print('Problem: 54, Chapter: 08, Book: "Practical Social Network Analysis with Python"\n')
-print()
+print('Simulate the effect of the high roller dinner on the two graphs. First, read in\n\
+the graphs and assign the initial configuration as before. Now, before the decision\n\
+process, you spend Rs. k on the fancy dinner and then go through the decision process\n\
+of counting votes.')
