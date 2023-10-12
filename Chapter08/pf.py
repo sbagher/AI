9e2edac7cd2dc832108415591252bf4015f0678d
sbagher <sbagher@ut.ac.ir>
2023-10-12 10:30:08 +0330

    Delete pf.py

--- Chapter08/pf.py
+++
@@ -1,4 +0,0 @@
-# Name: Saeed Baghershahi
-# Student Number: 102501002
-# Class: AI Applications in Social Networks
-# Assignment: Problem: Final, Chapter: 08, Book: "Practical Social Network Analysis with Python"
