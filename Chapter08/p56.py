1d3aee6a88ac07b7a6eef07230a5baba64250f0e
sbagher <sbagher@ut.ac.ir>
2023-10-13 13:14:47 +0330

    Update p56.py

--- Chapter08/p56.py
+++ Chapter08/p56.py
@@ -3,19 +3,12 @@
 # Class: AI Applications in Social Networks
 # Assignment: Problem: 56, Chapter: 08, Book: "Practical Social Network Analysis with Python"
 
-import networkx as nx
-import numpy as np
+from collections import Counter
 
 print('Problem: 56, Chapter: 08, Book: "Practical Social Network Analysis with Python"\n')
 print('Using the N calculated in Problem 55, compute its cumulative histogram [N] =\n\
 (N[1], . . . , N[n−1]), where N[k] = k l=0 Nl . Plot the cumulative histogram and report\n\
 the final number of rioters.')
-
-
-from collections import Counter
-
-print('Problem: 55, Chapter: 08, Book: "Practical Social Network Analysis with Python"\n')
-print('For this threshold vector, compute the histogram N.')
 
 T = [1, 1, 1, 1, 1, 4, 1, 0, 4, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 4, 0, 1, 4, 0, 1, 1, 1, 
      4, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 4, 1, 1, 4, 1, 4, 
@@ -29,4 +22,11 @@ for i in range (max(C.keys())+1):
         N.append(C[i])
     else:
         N.append(0)
+
+NS = [N[0]]
+for i in range (1, max(C.keys())+1):
+    NS.append(NS[i-1]+N[i])
+NS = NS[1:]
+
 print (f'N = {N}')
+print (f'NS = {NS}')
