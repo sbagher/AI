8f01a6a783d09fd502d9bbd9893759fdf62c2d0a
sbagher <sbagher@ut.ac.ir>
2023-10-13 13:02:55 +0330

    Update p55.py

--- Chapter08/p55.py
+++ Chapter08/p55.py
@@ -4,8 +4,21 @@
 # Assignment: Problem: 55, Chapter: 08, Book: "Practical Social Network Analysis with Python"
 
 
-import networkx as nx
-import numpy as np
+from collections import Counter
 
 print('Problem: 55, Chapter: 08, Book: "Practical Social Network Analysis with Python"\n')
 print('For this threshold vector, compute the histogram N.')
+
+T = [1, 1, 1, 1, 1, 4, 1, 0, 4, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 4, 0, 1, 4, 0, 1, 1, 1, 
+     4, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 4, 1, 1, 4, 1, 4, 
+     0, 1, 0, 1, 1, 1, 0, 4, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 
+     1, 0, 4, 0, 4, 0, 0, 1, 1, 1, 4, 0, 4, 0]
+C = Counter(T)
+#C = sorted(C.items(), key=lambda x: (x[0], x[1]))
+N = []
+for i in range (max(C.keys())+1):
+    if i in C.keys():
+        N.append(C[i])
+    else:
+        N.append(0)
+print (f'N = {N}')
